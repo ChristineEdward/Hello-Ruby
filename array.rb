5e0e77e51dde97ffe3f9acb203ae5b51e6b2cfce
ChristineEdward <christine12@gmail.com>
2020-06-06 06:41:58 +0630

    my new

---
+++ array.rb
@@ -0,0 +1,15 @@
+puts "     	 |+|              "
+puts " ______|+|              "
+puts " ++++++|+|++++++++      "    
+puts "       |+|              "
+puts "       |+|              "
+
+items = ["Apple","Banana","Orange"]
+items << 8
+items.push(1)
+items.insert(5,3)
+puts items 
+puts ......
+a = [2,3,7,8]
+b = [2,7,9]
+puts a & b 
